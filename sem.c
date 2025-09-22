5bbd94281293b4eb6280d1db2a85a2547add26bf
Brillerd <brillarysamwel@gmail.com>
2025-09-22 16:43:42 +0300

    Add files via upload

---
+++ sem.c
@@ -0,0 +1,18 @@
+/*
+Name:Brillary Samuel
+Reg no:PA106/G/28798/25
+Description: Program to display all units semester one
+*/
+
+#include <stdio.h>
+int main() {
+	printf("SPM 2100 Discrete Mathematics \n");
+	printf ("SPM 2104 Mathematics for Science \n");
+	printf ("UCU 2105 Fundamentals of ICT \n");
+	printf ("SPI 2102 Computer System Organization \n");
+	printf ("UCU 2101 Communication Skills \n");
+	printf ("SPC 2101 Introduction to Programming & Probem Solving \n");
+	printf ("2101 Fundamentals of System Analysis & Design \n");
+		return 0;
+}                  
+  
